51bfaffc0b236fff0cb1bede510b86aab35add7e
Zhenhui Sun <zhenhui.sun@intel.com>
2024-01-12 10:17:32 +0800

    Update llama.py

--- llm/llama.py
+++ llm/llama.py
@@ -103,13 +103,10 @@ first_latency = np.mean(vFristTokenList)
 average_2n_latency = np.mean(vNextTokenList)
 average_2n = vNextTokenList
 average_2n.sort()
-if batch_size<32:
-    p90_index = int(len(average_2n) * 0.5)
-else:
-    p90_index = int(len(average_2n) * 0.9)
+p90_index = int(len(average_2n) * 0.9)
 p99_index = int(len(average_2n) * 0.99)
-print(p90_index)
-print(p99_index)
+# print(p90_index)
+# print(p99_index)
 p90_latency = average_2n[p90_index]
 p99_latency = average_2n[p99_index]
 print("First token average latency: %.3f sec." % first_latency)
